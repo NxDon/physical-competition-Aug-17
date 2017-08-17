b82bda6839a95448ffd8d2d6c98f305abf72bf1a
NxDon <645104928@qq.com>
2017-08-17 14:06:20 +0800

    finish task

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+为数组 arr 中的每个元素求二次方。不要直接修改数组 arr，结果返回新的数组
+例如：arr = [5, 3, 7, 1]; 输出：[25, 9, 49, 1]
+要求：不要使用for，while循环
+*/
+function getArrPowered(arr) {
+    var copyedArr = arr.concat();//deep copy
+    return copyedArr.map((elem) => Math.pow(elem,2))
+}
+arr = [5,3,7,1]
+console.log(`arr = [5,3,7,1]\ngetArrPowered(arr) :[${getArrPowered(arr)}]\narr = [${arr}]`);
