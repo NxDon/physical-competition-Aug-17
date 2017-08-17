004390a382da0015c1453096ee7144d85061eaf5
NxDon <645104928@qq.com>
2017-08-17 14:16:15 +0800

    add currying function

--- index.js
+++ index.js
@@ -4,7 +4,19 @@
 要求：不要使用for，while循环
 */
 function getArrPowered(arr) {
-    return arr.concat().map((elem) => Math.pow(elem,2))
+    return arr.concat().map((elem) => Math.pow(elem, 2))
 }
-arr = [5,3,7,1]
-console.log(`arr = [5,3,7,1]\ngetArrPowered(arr) :[${getArrPowered(arr)}]\narr = [${arr}]`);
+
+
+//  拓展 getArrPoweredCurring柯里化了getArrPowered，接收一个次方，返回函数可以求数组每个元素的power次方
+/*
+function getArrPoweredCurrying(power) {
+    return (arr) => {
+        return arr.concat().map((elem) => Math.pow(elem, power))
+    }
+}
+var p2 = getArrPoweredCurrying(3);
+arr = [1,2,3,4];
+p2(arr);
+
+ */
